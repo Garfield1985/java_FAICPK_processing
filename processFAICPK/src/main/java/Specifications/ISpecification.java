d03d5cd76859a1cefb2158aa2761eae3cf099e60
Garfield <>
2022-05-29 19:15:16 +0800

    Add the Meta Data class and tests. Add teh Specification class and tests. 	The specification class would not sepearate the single side/double side tolerances. Add the draft dimension record class and tests.

---
+++ processFAICPK/src/main/java/Specifications/ISpecification.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Specifications;
+
+/**
+ *
+ * @author fatca
+ */
+public interface ISpecification {
+
+    DimensionCatalog getDimension_catalog();
+
+    String getDimension_number();
+
+    DimensionType getDimension_type();
+
+    double getLSL();
+
+    double getNegative_tolerance();
+
+    double getNominal();
+
+    double getPositive_tolerance();
+
+    double getUSL();
+    
+}
